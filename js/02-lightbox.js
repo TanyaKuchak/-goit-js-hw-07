1b72c37b1392d487d4f94b2c4a866c39c8a98407
Tanya Kuchak <tanyakuchak@ukr.net>
2022-09-15 08:56:57 +0300

    done 07

---
+++ js/02-lightbox.js
@@ -0,0 +1,26 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsMarkup = createGalerryItemsMarkup(galleryItems);
+
+galleryEl.innerHTML = galleryItemsMarkup;
+
+function createGalerryItemsMarkup(gallery) {
+  return gallery.map(({ preview, original, description }) => {
+      return `
+      <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+      `;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay:200,
+});
+
